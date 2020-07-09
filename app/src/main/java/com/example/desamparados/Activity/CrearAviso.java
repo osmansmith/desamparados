530fc391cbc9f41c18719f38fd750383549bf1c6
melon <ememendeztabilo@gmail.com>
2020-07-09 19:43:12 -0400

    camera implementation

--- app/src/main/java/com/example/desamparados/Activity/CrearAviso.java
+++ app/src/main/java/com/example/desamparados/Activity/CrearAviso.java
@@ -1,15 +1,34 @@
 package com.example.desamparados.Activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.desamparados.MapsActivity;
 import com.example.desamparados.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CrearAviso extends AppCompatActivity implements View.OnClickListener {
     public static double latitude;
@@ -17,7 +36,15 @@ public class CrearAviso extends AppCompatActivity implements View.OnClickListene
     public static EditText p_latitude;
     public static EditText p_longitude;
     public static EditText p_address;
+    Button btn_img;
     Button btn_map;
+    ImageView img;
+
+
+    public static final int REQUEST_CODE_TAKE_PHOTO = 0 /*1*/;
+    private Uri photoURI;
+    public String mCurrentPhotoPath;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +55,9 @@ public class CrearAviso extends AppCompatActivity implements View.OnClickListene
         p_address=(EditText) findViewById(R.id.p_address);
         btn_map = (Button) findViewById(R.id.btn_map) ;
         btn_map.setOnClickListener(this);
+        btn_img = (Button) findViewById(R.id.btn_img);
+        btn_img.setOnClickListener(this);
+        img = (ImageView) findViewById(R.id.img);
 
     }
 
@@ -35,10 +65,156 @@ public class CrearAviso extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+            case R.id.btn_img:
+                showOptions();
+                break;
             case R.id.btn_map:
                 Intent i = new Intent(getApplicationContext(), MapsActivity.class);
                 startActivity(i);
                 break;
         }
     }
+
+
+
+    private void showOptions() {
+        final CharSequence[] option = {"Tomar foto", "Elegir de galeria", "Cancelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(CrearAviso.this);
+        builder.setTitle("Eleige una opción");
+        builder.setItems(option, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (option[which] == "Tomar foto") {
+                    openCamera();
+                } else if (option[which] == "Elegir de galeria") {
+                    openGallery();
+
+                } else {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        builder.show();
+    }
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "MyPicture");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "Photo taken on " + System.currentTimeMillis());
+                photoURI = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                //Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), "com.example.android.fileprovider", photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_CODE_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    public void openCamera() {
+
+
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            225);
+                }
+
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.CAMERA)) {
+
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CAMERA},
+                            226);
+                }
+            } else {
+                dispatchTakePictureIntent();
+            }
+
+        }
+
+
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,100 );
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath= image.getAbsolutePath();
+        return image;
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
+                img.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(requestCode==100){
+            photoURI = data.getData();
+            img.setImageURI(photoURI);
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            img.setImageBitmap(bitmap);
+
+        }
+    }
+
+
 }
