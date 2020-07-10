f5f765b05bb3d0d30d2ad0bad31c3d34e3568e1a
melon <ememendeztabilo@gmail.com>
2020-07-10 17:20:01 -0400

    clases relacionadas con aviso generadas

---
+++ app/src/main/java/com/example/desamparados/Clases/TipoMascota.java
@@ -0,0 +1,27 @@
+package com.example.desamparados.Clases;
+
+public class TipoMascota {
+    private int id;
+    private String nombre;
+
+    public TipoMascota(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
