ef232a88a274c2e44753fc00e80b1c1c907dac02
karen371 <karengodoy325@gmail.com>
2020-07-14 19:13:20 -0400

    actualizacion de login

---
+++ app/src/main/java/com/example/desamparados/Activity/Iniciar_sesion.java
@@ -0,0 +1,16 @@
+package com.example.desamparados.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.desamparados.R;
+
+public class Iniciar_sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
---
+++ app/src/main/java/com/example/desamparados/Activity/Registrar.java
@@ -0,0 +1,15 @@
+package com.example.desamparados.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.desamparados.R;
+
+public class Registrar extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
